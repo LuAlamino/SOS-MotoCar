20230a1474e2877de6d9cbf712de4438fc273fcf
Lucas Alamino <alaminolucas@gmail.com>
2024-09-24 17:53:10 -0300

    Config uwsgi

--- app/settings.py
+++ app/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
